e15298b59b9393a1b6209a4be06c0d5cc0d3bc5a
Salman Hussain <salman.hussain@live.co.uk>
2021-03-01 20:29:27 +0000

    Footer.js: updated Paragraph

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <p>&copy; Finfo 20201.  All Rights Reserved.</p>
+        <p>&copy; Finfo 2021.  All Rights Reserved.</p>
         <ul className="list-inline">
           <li className="list-inline-item">
             <a href="/#">Privacy</a>
